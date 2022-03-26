e480fc1eb23950d4218a5a86bb23534e644ecf8c
mahmutboran <86852026+mahmutboran@users.noreply.github.com>
2022-03-26 18:44:11 +0300

    first

---
+++ src/components/item/Item.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+import "./Item.css"
+
+const Item = ({card}) => { //!props.card daolabilr süslüsüz
+  const [showLogo,setShowLogo] = useState(true)
+   
+  
+  return (
+    <div className='card' onClick={()=>setShowLogo(!showLogo)}>
+       {showLogo ? (
+       <div> 
+        <img className='card-logo' src={card.img} alt="logo" />
+        <h3 className='card-title'>{card.name}</h3>
+        </div> 
+        )
+        :
+        (
+ 
+    <ul className='list'>
+        {
+            card.options.map((opt,index)=>{
+                return <li key={index}>{opt}</li>
+            })
+        }
+
+    </ul>)}
+    </div>
+  )
+}
+
+export default Item
